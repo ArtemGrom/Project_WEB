0f9a257addb772e53a02c117bd290f5f532f6e3a
AG <art.gromov@bk.ru>
2021-10-27 11:53:15 +0300

    Add register users

--- app/users/views.py
+++ app/users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def register(request):
+    """Регистрация нового пользователя"""
+    if request.method != 'POST':
+        # Отображаем пустую регистрационную форму
+        form = UserCreationForm()
+    else:
+        # Обрабатываем запрошенную форму
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # Залогиниваем пользователя и затем перенаправляем на домашнюю страницу
+            login(request, new_user)
+            return redirect('learning_logs:index')
+
+    # Отображение пустой или невалидной формы
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
